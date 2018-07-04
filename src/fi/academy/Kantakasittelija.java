9981c6a7227a5441306bad9f6889c6d5053bb758
nikopit <niko.pitkonen@helsinki.fi>
2018-07-04 16:13:57 +0300

    yhteydenluonti

--- src/fi/academy/Kantakasittelija.java
+++ src/fi/academy/Kantakasittelija.java
@@ -1,6 +1,21 @@
 package fi.academy;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Kantakasittelija {
 
+    public void luoYhteys() {
+        try {
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/tenttikysymykset?useSSL=false&serverTimezone=UTC", "root", "salasana");
+            System.out.println("Yhteys onnistui!");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
+    public void haeTiedot() {
+        
+    }
 }
